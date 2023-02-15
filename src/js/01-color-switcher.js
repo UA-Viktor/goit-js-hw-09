155de1134cabc411f0dc578ead332bfe9ccfcdae
UA-Viktor <v.korenkov@icloud.com>
2023-02-15 19:48:37 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,29 +2,26 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 };
 
-const refs = {
-  body: document.querySelector("body"),
-  start: document.querySelector('[data-start]'),
-  stop: document.querySelector('[data-stop]'),
-};
+const bodyEl = document.querySelector("body");
+const startEl = document.querySelector('[data-start]');
+const stopEl = document.querySelector('[data-stop]');
 
-refs.start.addEventListener('click', start);
-refs.stop.addEventListener('click', stop);
+startEl.addEventListener('click', start);
+stopEl.addEventListener('click', stop);
 
 let intervalId;
-refs.stop.setAttribute('disabled', '');
+stopEl.setAttribute('disabled', '');
 
 function start() {
   intervalId = setInterval(() => {
-    refs.body.style.backgroundColor = getRandomHexColor();
+    bodyEl.style.backgroundColor = getRandomHexColor();
   }, 1000);
-
-  refs.start.setAttribute('disabled', '');
-  refs.stop.removeAttribute('disabled', '');
+  startEl.setAttribute('disabled', '');
+  stopEl.removeAttribute('disabled', '');
 };
 
 function stop() {
   clearInterval(intervalId);
-  refs.stop.setAttribute('disabled', '');
-  refs.start.removeAttribute('disabled', '');
+  stopEl.setAttribute('disabled', '');
+  startEl.removeAttribute('disabled', '');
 };
